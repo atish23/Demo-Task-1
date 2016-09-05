5ca1320aa8fd3448d74469a2454688581f17e675
Atish <amaske@gmail.com>
2016-09-06 03:01:31 +0530

    Heroku setup

--- db/migrate/20160902132320_change_data_type_for_status.rb
+++ db/migrate/20160902132320_change_data_type_for_status.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypeForStatus < ActiveRecord::Migration[5.0]
   def change
-    ALTER TABLE "todos" ALTER COLUMN "status" TYPE integer
+    execute "ALTER TABLE "todos" ALTER COLUMN "status" TYPE integer"
   end
 end

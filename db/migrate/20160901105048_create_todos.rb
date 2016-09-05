38b2606b844ddf55a4a3d00b4208f5160b5738f2
Atish <amaske@gmail.com>
2016-09-06 03:12:13 +0530

    Heroku setup

--- db/migrate/20160901105048_create_todos.rb
+++ db/migrate/20160901105048_create_todos.rb
@@ -2,9 +2,8 @@ class CreateTodos < ActiveRecord::Migration[5.0]
   def change
     create_table :todos do |t|
       t.string :name
-      t.integer :status
+      t.string :status
       t.integer :project_id
-
       t.timestamps
     end
   end
--- db/migrate/20160902132320_change_data_type_for_status.rb
+++ db/migrate/20160902132320_change_data_type_for_status.rb
@@ -1,5 +1,5 @@
-# class ChangeDataTypeForStatus < ActiveRecord::Migration[5.0]
-#   def change
-#     change_column(:todos, :status, :integer)
-#   end
-# end
+class ChangeDataTypeForStatus < ActiveRecord::Migration[5.0]
+  def change
+    change_column(:todos, :status, 'integer USING CAST(status AS integer)')
+  end
+end
